56ab43da3c152b4651b07a59cf324f4287dbbd7b
grimesj1038 <91433438+grimesj1038@users.noreply.github.com>
2021-10-03 16:39:29 -0400

    Add files via upload
    Caffeine Levels Exercise

---
+++ P2LAB2_Caffeine_GrimesJasmine.py
@@ -0,0 +1,6 @@
+caffeine_mg = float(input())
+
+print("After 6 hours: {:.2f} mg".format(caffeine_mg / 2))
+print("After 12 hours: {:.2f} mg".format(caffeine_mg / 4))
+print("After 24 hours: {:.2f} mg".format(caffeine_mg / 16))
+
